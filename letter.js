0e012e4eec1e766bd6c7bd8edcebc5cb84589068
Your Name <nbos0531@gmail.com>
2018-02-11 01:18:19 -0600

    changes1

--- letter.js
+++ letter.js
@@ -4,10 +4,10 @@ var Letter = function (letter) {
 	this.letterDisplay = function() {
 		//console.log('hi');
 		if (this.display === false) {
-			return "__ ";
+			return " __ ";
 		}
 		else {
-			console.log(this.letter);
+			return this.letter;
 		}
 	}
 }
--- word.js
+++ word.js
@@ -1,24 +1,49 @@
 var Letter = require('./Letter');
 
-var Word = function(w) {
-	this.myWord = w.split('');
-	console.log(this.myWord);
+var Word = function(w, char) {
+	this.chosenWordArray = w.split('');
 	this.words = [];
+	this.character = char;
+
+	// create blanks for word
 	this.blankWords = function() {
-		for (var i = 0; i < this.myWord.length; i++) {
-		var newWord = new Letter(this.myWord[i]);
-		this.words.push(newWord.letterDisplay());
+		//Loop through chosenword array
+		for (var i = 0; i < this.chosenWordArray.length; i++) {
+			//create object with each letter in chosen word
+			var newWord = new Letter(this.chosenWordArray[i]);
+			// creates the blanks for letters to be guessed and stores them in array
+			this.words.push(newWord.letterDisplay());
+		}
+	console.log(this.words.join(' '));
 
 	}
-	console.log(this.words);
 
+	//check if correct guess
+	this.checkCharacters = function() {
+		console.log(this.chosenWordArray);
+		console.log(this.character);
+		if (this.chosenWordArray.indexOf(this.character) > -1) {
+		console.log("Winner winner chicken dinner");
+			for (var i = 0; i < this.chosenWordArray.length; i++) {
+				if (this.chosenWordArray[i] === this.character) {
+					this.words[i] = this.character;
+					console.log(this.words.join(' '));
+				}
+
+
+				// var newWord = new Letter(this.chosenWordArray[i]);
+				// newWord.display = true;
+				// newWord.letterDisplay();
+				// console.log(newWord);
+			}
+		}
+		else {
+			console.log("you suck");
+		}
+	}
 
 }
 
-
-
-
-}
-
-var mine = new Word('today');
+var mine = new Word('today', "t");
 mine.blankWords();
+mine.checkCharacters();
